391e64cbc41831e6860632587b8be3fdc08ab48f
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-07-05 12:54:48 +0200

    Use a JSON file for survey repository

--- src/atoms/survey/survey.repository.ts
+++ src/atoms/survey/survey.repository.ts
@@ -1,24 +1,46 @@
 import { injectable } from 'inversify';
-import { Observable, of } from 'rxjs';
+import { bindCallback, Observable } from 'rxjs';
 import { Survey, SurveyCreationRequest } from './data/survey.models';
 import { uuid } from 'uuidv4';
+import * as fs from 'fs';
+import { flatMap } from '@rxjsx/rxjsx';
+import { map } from 'rxjs/operators';
 
 @injectable()
 export class SurveyRepository {
-  private surveys: Map<string, Survey>;
+  private readonly dumpPath: string;
 
   constructor() {
-    this.surveys = new Map<string, Survey>();
+    this.dumpPath = 'surveys.json';
   }
 
   public save(request: SurveyCreationRequest): Observable<Survey> {
     const id = uuid();
     const survey: Survey = { ...request, id };
-    this.surveys.set(id, survey);
-    return of(survey);
+    return this.loadData().pipe(
+      flatMap((data) => {
+        return this.dumpData({ ...data, [id]: survey }).pipe(map(() => survey));
+      }),
+    );
   }
 
   public getById(id: string): Observable<Survey> {
-    return of(this.surveys.get(id));
+    return this.loadData().pipe(map((surveys) => surveys[id]));
+  }
+
+  private loadData(): Observable<Record<string, Survey>> {
+    return bindCallback(fs.readFile)(this.dumpPath).pipe(
+      map(([errno, buffer]) => {
+        try {
+          return JSON.parse(buffer.toString());
+        } catch (e) {
+          return {};
+        }
+      }),
+    );
+  }
+
+  private dumpData(data: Record<string, Survey>): Observable<boolean> {
+    return bindCallback(fs.writeFile)(this.dumpPath, JSON.stringify(data)).pipe(map((_) => true));
   }
 }
