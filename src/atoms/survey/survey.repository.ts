2ba4f3ae3f3413a98c539e8e06a51c1792eda92d
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-07-05 13:58:17 +0200

    Extract disk functions as a separate module

--- src/atoms/survey/survey.repository.ts
+++ src/atoms/survey/survey.repository.ts
@@ -1,46 +1,28 @@
 import { injectable } from 'inversify';
-import { bindCallback, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Survey, SurveyCreationRequest } from './data/survey.models';
 import { uuid } from 'uuidv4';
-import * as fs from 'fs';
 import { flatMap } from '@rxjsx/rxjsx';
 import { map } from 'rxjs/operators';
+import { dumpData, loadData } from '../../utils/disk';
 
 @injectable()
 export class SurveyRepository {
   private readonly dumpPath: string;
 
   constructor() {
-    this.dumpPath = 'surveys.json';
+    this.dumpPath = 'surveys.db.json';
   }
 
   public save(request: SurveyCreationRequest): Observable<Survey> {
     const id = uuid();
     const survey: Survey = { ...request, id };
-    return this.loadData().pipe(
-      flatMap(data => {
-        return this.dumpData({ ...data, [id]: survey }).pipe(map(() => survey));
-      }),
-    );
+    return loadData<Record<string, Survey>>(this.dumpPath)
+      .pipe(flatMap(data => dumpData(this.dumpPath, { ...data, [id]: survey })))
+      .pipe(map(() => survey));
   }
 
   public getById(id: string): Observable<Survey> {
-    return this.loadData().pipe(map(surveys => surveys[id]));
-  }
-
-  private loadData(): Observable<Record<string, Survey>> {
-    return bindCallback(fs.readFile)(this.dumpPath).pipe(
-      map(([errno, buffer]) => {
-        try {
-          return JSON.parse(buffer.toString());
-        } catch (e) {
-          return {};
-        }
-      }),
-    );
-  }
-
-  private dumpData(data: Record<string, Survey>): Observable<boolean> {
-    return bindCallback(fs.writeFile)(this.dumpPath, JSON.stringify(data)).pipe(map(_ => true));
+    return loadData(this.dumpPath).pipe(map(surveys => surveys[id]));
   }
 }
